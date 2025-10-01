8c0c5c0aeb5a0bdfab9ebc2c2793d1fbc5f305bc
Himanshu Kumar Singh <140483011+himanshusingh9576@users.noreply.github.com>
2025-10-01 10:36:05 +0000

    done

--- loops.py
+++ loops.py
@@ -28,20 +28,24 @@ while m<=10:
 """
 
 # Print the elements of the following list using a loop
+"""
 ele2 = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 ele3= 0
 
 while ele3<len(ele2):
  
     print(ele2[ele3])
-    ele3 += 1   
+    ele3 += 1    """
+
+
 # Print the element of the following list using loop
-heroes = ["ironman", "thor", "superman", "batman"]
-idx = 0
-while idx < len(heroes):
-    print(heroes[idx])
-    idx += 1
+# heroes = ["ironman", "thor", "superman", "batman"]
+# idx = 0
+# while idx < len(heroes):
+#     print(heroes[idx])
+#     idx += 1
 # Search for a number x in this tuple using loop:
+"""
 elex = (1, 4, 16, 25, 36, 49, 64, 81, 100)
 elen = 36
 eley = 0
@@ -49,3 +53,36 @@ while eley < len(elex):
     if elex[eley] == elen:
         print(eley)
     eley += 1
+    """
+
+# Break and Continue Keyword
+'''
+i = 1
+while i <= 5:
+    print(i)
+    if i==3:
+        break
+    i += 1 '''
+
+#Continue
+# i = 0 
+# while i <= 5:
+#     if(i==3):
+#         i += 1
+#         continue
+#     print(i)
+#     i += 1
+# Continue skip that mention element in if condition
+l = 2
+while l<=10:
+    
+    if l==5:
+        l += 1
+        continue
+        
+    print(l)
+    l += 1
+
+# For loops in python
+
+   
