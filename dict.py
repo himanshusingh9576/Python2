a1aaab400eacc240ee7ed3a037b9e22520f2fea8
Himanshu Kumar Singh <140483011+himanshusingh9576@users.noreply.github.com>
2025-10-01 06:14:56 +0000

    done

--- dict.py
+++ dict.py
@@ -49,9 +49,29 @@ print(store)
 # you are a given list of subject for student
 # Assume one classroom is required for 1 subject
 #How many classroom are needed by all students
-classroom = {
-    "python", "java", "c++", "python",
-    "javascript", "java", "python", "java",
-    "c++", "c"
-}
-print(len(classroom))
+# classroom = {
+#     "python", "java", "c++", "python",
+#     "javascript", "java", "python", "java",
+#     "c++", "c"
+# }
+# print(len(classroom))
+
+# WAP to enter marks of 3 subject from the user
+# and store them in a dictionary. start with
+# an empty dictionary & add by one. Use subject
+# name as key & marks as value
+"""
+stu1 = int(input("Enter Your Maths Marks:- "))
+stu2 = int(input("Enter Your Phy Marks:- "))
+stu3 = int(input("Enter Your Chem Marks:- "))
+dict ={}
+dict.update({"Maths": stu1})
+dict.update({"Phy": stu2})
+dict.update({"Chem": stu3})
+print(dict)  """
+
+# Figure out a way to store 9 & 9.0 as separate
+# values in the set (you can take help of built-in data types)
+set = {9, 9.0}
+print(set)
+ 
